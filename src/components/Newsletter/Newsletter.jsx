08d4752b383002aefb07e3d3e730bb1c3415273b
deadlycow-andreas <pew.andreas.k@gmail.com>
2024-10-27 16:45:00 +0100

    started with newsletter section

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './newsletter.css';
+import notificationIcon from '../../assets/icons/notification-icon.svg';
+import mailIcon from '../../assets/icons/mail-icon.svg';
 
 function Newsletter() {
   return (
-    <div>
-      
+    <div className='newsletter'>
+      <div className='newsletter-header'>
+        <img src={notificationIcon}/>
+        <h4>Subscribe to our newsletter</h4>
+        {/* <h4>to stay informed about latest updates</h4> */}
+      </div>
+      <form>
+        <input placeholder='Your email' type="email" />
+        <img src={mailIcon}/>
+        <button className='btn'>Subscribe</button>
+      </form>
     </div>
   )
 };
